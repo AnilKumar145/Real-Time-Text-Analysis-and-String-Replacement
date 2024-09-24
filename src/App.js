d549eb276ddbb5c0dd894bbfd050f5b070a7a7a8
Sala Anil Kumar <anilkumarsala2003@gmail.com>
2024-09-24 11:38:53 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TextAnalysis from './components/TextAnalysis';
+import './App.css'; // Ensure you import your CSS file
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title-container">
+        <h1>Real-Time Text Analysis and String Replacement</h1>
+      </div>
+      <TextAnalysis />
     </div>
   );
 }
